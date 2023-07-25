3697b100569d944d6560f49a2339e14bc3b31b18
Thibaut Gautier <shanyujuneja@gmail.com>
2023-07-25 13:28:40 +0200

    fix: background img

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -14,8 +14,8 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Fairy Framework was designed from the ground up to be easily installed and
+        used to get your software up and running quickly.
       </>
     ),
   },
@@ -24,18 +24,18 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Fairy Framework lets you focus on what matters, and we&apos;ll do the chores. Go
+        ahead and start your journey as a 10x engineer today.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Powered by Reflections',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Fully dynamic, optimized, with primary focus on extendability. Whether for Spigot,
+        small applications, or bots, Fairy is perfect for you.
       </>
     ),
   },
